1e0c80286fe4385ef8c91a75bfadb839a1453445
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2021-02-19 10:19:53 +0000

    first_plot

--- analytics.py
+++ analytics.py
@@ -23,6 +23,37 @@ connection_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_na
 engine = db.create_engine(connection_str)
 connection = engine.connect()
 
-users = pd.read_sql("SELECT user_id, name, sign_up FROM users", connection)
+# plot user number statistics
+def plot_user_numbers():
+    sign_ups = pd.read_sql("SELECT sign_up FROM users", connection)
+    sign_outs = pd.read_sql("SELECT cancelled FROM users WHERE cancelled IS NOT NULL", connection)
 
-print(users)
+    sign_ups['user_growth'] = 1
+    sign_outs['user_growth'] = -1
+    
+    sign_ups.rename(columns={'sign_up' : 'dates'}, inplace=True)
+    sign_outs.rename(columns={'cancelled' : 'dates'}, inplace=True)
+    
+    print(sign_ups)
+    print(sign_outs)
+
+    user_change = [sign_ups, sign_outs]
+
+    user_change = pd.concat(user_change)
+    user_change = user_change.sort_values(by='dates')
+    user_change = user_change.reset_index(drop=True)
+    print(user_change)
+
+    compound = []
+
+    for i in range(len(user_change.index)):
+        compound.append(user_change['user_growth'].iloc[:i+1].sum())
+
+    user_change['compounds'] = compound
+
+    print(user_change)
+
+    sns.lineplot(x = 'dates', y = 'compounds', data = user_change) 
+    plt.savefig('user_numbers.png')
+
+plot_user_numbers()
